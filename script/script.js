f90e2af8ce5eef08651207199233abe5197828f7
maric33 <161638899+maric33@users.noreply.github.com>
2024-03-06 15:26:39 +0100

    Mostly finished game!

--- script/script.js
+++ script/script.js
@@ -1,13 +1,33 @@
 const comp = ['rock', 'paper', 'scissors'];
-const player = 'paper';
 
 
 function getComputerChoice(){
     const randomize = Math.floor(Math.random() *3);
     const choice = comp[randomize];
-    console.log(randomize);
-    console.log(choice);
-    
+    return choice;
+    }
+
+function playGame(getComputerChoice, playerChoice){
+    if (getComputerChoice === playerChoice) {
+        return 'tie';
+    } else if (playerChoice === 'rock' && getComputerChoice === 'scissors' || playerChoice === 'scissors' && getComputerChoice === 'paper' || playerChoice === 'paper' && getComputerChoice ==='rock') {
+        return 'player';
+    } else  {
+        return 'computer';
+    }
 }
 
-getComputerChoice();
+let win = playGame(getComputerChoice(), 'rock');
+
+function gameResult() {
+    if (win === 'tie'){
+    console.log('Its tie. Nobody Wins'); 
+} else if (win === 'player'){
+    console.log('Player wins!');  
+} else {
+    console.log('Computer wins this time!')
+}
+}
+console.log(win);
+console.log(gameResult());
+
